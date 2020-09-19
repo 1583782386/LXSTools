bbdcef456fb3366c5d550d193243928f2355f7db
1583782386@qq.com <1583782386@qq.com>
2020-09-19 10:13:25 +0800

    ‘库文件’

---
+++ LXSTools/LXSLogDebugKit.h
@@ -0,0 +1,17 @@
+//
+//  LXSLogDebugKit.h
+//  Demo_Pods
+//
+//  Created by delegate on 2020/9/8.
+//  Copyright © 2020 yunxue. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LXSLogDebugKit : NSObject
+- (void) debugPrint;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LXSTools/LXSLogDebugKit.m
@@ -0,0 +1,16 @@
+//
+//  LXSLogDebugKit.m
+//  Demo_Pods
+//
+//  Created by delegate on 2020/9/8.
+//  Copyright © 2020 yunxue. All rights reserved.
+//
+
+#import "LXSLogDebugKit.h"
+
+@implementation LXSLogDebugKit
+- (void) debugPrint
+{
+    NSLog(@"日志打印");
+}
+@end
